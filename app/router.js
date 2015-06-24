637fc7c1be7829f46e9dd56ea091dbdef8fdc5c1
Pablo Brasero <pablo@new-bamboo.co.uk>
2015-06-24 17:49:16 +0100

    Route to new functionality

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('friends', function() {});
+
+  this.route('friends', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
