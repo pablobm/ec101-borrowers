0624a30175103f9c28c19f3415f9875be1178198
Pablo Brasero <pablo@pablobm.com>
2015-06-29 16:30:56 +0100

    Articles require a description

---
+++ app/controllers/articles/new.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.description',
+    function() {
+      return !Ember.isEmpty(this.get('model.description'));
+    }
+  ),
+
+  actions: {
+    save: function() {
+      if (this.get('isValid')) {
+        return true;
+      }
+      else {
+        this.set('errorMessage', "Please provide a description");
+        return false;
+      }
+    },
+  },
+});
