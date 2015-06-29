75979281b3d13826c0fa9edc16d1e9aea0dbfe42
Pablo Brasero <pablo@pablobm.com>
2015-06-29 23:07:43 +0100

    BUG: helpers receive arguments as a single array

--- app/helpers/formatted-date.js
+++ app/helpers/formatted-date.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import { formatDate } from '../utils/date-helpers';
 
-export default Ember.HTMLBars.makeBoundHelper(function(date, format) {
+export default Ember.HTMLBars.makeBoundHelper(function([date, format]) {
   return formatDate(date, format);
 });
