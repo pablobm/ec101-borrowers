84a80cc72f421689b24bc5add6a9c1eeeeee941d
Pablo Brasero <pablo@pablobm.com>
2015-06-29 17:10:22 +0100

    Actually handle the 'save' action

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -4,4 +4,11 @@ export default Ember.Route.extend({
   model: function() {
     return this.modelFor('friends/show').get('articles');
   },
+
+  actions: {
+    save: function(model) {
+      model.save();
+      return false;
+    },
+  },
 });
