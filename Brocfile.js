838d6909c3c9b968cbe97598cc4201aea14e0a6d
Pablo Brasero <pablo@pablobm.com>
2015-06-29 20:00:48 +0100

    Import moment.js into project

--- Brocfile.js
+++ Brocfile.js
@@ -6,6 +6,7 @@ var app = new EmberApp();
 
 app.import('bower_components/picnic/releases/plugins.min.css');
 app.import('bower_components/picnic/releases/picnic.min.css');
+app.import('bower_components/moment/moment.js');
 
 app.import('vendor/fontello/fontello.css');
 app.import('vendor/fontello/font/fontello.ttf',  { destDir: 'font' });
