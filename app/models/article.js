294734d8f772cab3d38c0b1dafa232c4a98d44de
Pablo Brasero <pablo@pablobm.com>
2015-06-29 01:35:31 +0100

    Lend articles to friends

--- app/models/article.js
+++ app/models/article.js
@@ -4,6 +4,6 @@ export default DS.Model.extend({
   createdAt: DS.attr('date'),
   description: DS.attr('string'),
   nodes: DS.attr('string'),
-  state: DS.attr('string'),
+  state: DS.attr('string', { defaultValue: 'borrowed' }),
   friend: DS.belongsTo('friend'),
 });
--- app/router.js
+++ app/router.js
@@ -12,12 +12,12 @@ Router.map(function() {
     this.route('new');
 
     this.route('show', { path: ':friend_id' }, function() {
-      this.resource('articles', function(){});
+      this.resource('articles', function() {
+        this.route('new');
+      });
     });
 
-    this.route('edit', {
-      path: ':friend_id/edit'
-    });
+    this.route('edit', { path: ':friend_id/edit' });
   });
 });
 
