dd5d7e7f6f240a945c7cf46dbd72d4e0e301efc6
Pablo Brasero <pablo@pablobm.com>
2015-06-29 17:26:30 +0100

    Autosave changes to articles

--- app/components/articles/article-row.js
+++ app/components/articles/article-row.js
@@ -4,9 +4,18 @@ export default Ember.Component.extend({
   tagName: 'tr',
   article: null,
   articleStates: null,
-  actions: {
-    saveArticle: function(article) {
+
+  autoSave: function() {
+    var article = this.get('article');
+    if (!article.get('isNew')) {
       this.sendAction('save', article);
     }
-  }
+  },
+
+  stateChanged: Ember.on('init', Ember.observer('article.state', function() {
+    var article = this.get('article');
+    if (article.get('isDirty') && !article.get('isSaving')) {
+      Ember.run.once(this, this.autoSave);
+    }
+  })),
 });
