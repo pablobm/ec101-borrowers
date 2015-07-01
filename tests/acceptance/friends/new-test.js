3802bc2aa48042ef65f5cdd7a96a14027d243719
Pablo Brasero <pablo@new-bamboo.co.uk>
2015-07-01 16:14:49 +0100

    Example acceptance tests

--- tests/acceptance/friends/new-test.js
+++ tests/acceptance/friends/new-test.js
@@ -21,3 +21,30 @@ test('visiting /friends/new', function(assert) {
     assert.equal(currentURL(), '/friends/new');
   });
 });
+
+test("Creating a new friend", function(assert) {
+  visit('/');
+  click('a[href="/friends/new"]');
+  andThen(function() {
+    assert.equal(currentPath(), 'friends.new');
+  });
+
+  fillIn('input[placeholder="First name"]', 'Pinky');
+  fillIn('input[placeholder="Last name"]', 'Winky');
+  fillIn('input[placeholder="Email"]', 'pinky.winky@seriousbusiness.com');
+  fillIn('input[placeholder="Twitter"]', 'pwinky');
+  click('input[value="Save"]');
+
+  andThen(function() {
+    assert.equal(currentRouteName(), 'friends.show.index', "Redirects to friends.show after create");
+  });
+});
+
+test("Clicking save without filling fields", function(assert) {
+  visit('/friends/new');
+  click('input[value="Save"]');
+  andThen(function() {
+    assert.equal(currentRouteName(), 'friends.new', "Stays on new page");
+    assert.equal(find('h2:contains(You have to fill all the fields)').length, 1, "Displays error message");
+  });
+});
