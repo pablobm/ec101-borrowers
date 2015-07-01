f20768dfb10324de16e43e02c00b6f48e23556e7
Pablo Brasero <pablo@new-bamboo.co.uk>
2015-07-01 15:43:21 +0100

    Example unit tests

--- tests/unit/models/friend-test.js
+++ tests/unit/models/friend-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('friend', 'Unit | Model | friend', {
@@ -9,4 +10,23 @@ test('it exists', function(assert) {
   var model = this.subject();
   // var store = this.store();
   assert.ok(!!model);
+});
+
+test("fullName joins first and last name", function(assert) {
+  var model = this.subject({firstName: "Paquito", lastName: "Chocolatero"});
+  assert.equal(model.get('fullName'), "Paquito Chocolatero");
+
+  Ember.run(function() {
+    model.set('firstName', "Manolito");
+  });
+
+  assert.equal(model.get('fullName'), "Manolito Chocolatero", "Updates fullName");
+});
+
+test("articles relationship", function(assert) {
+  var klass = this.subject({}).constructor;
+  var relationship = Ember.get(klass, 'relationshipsByName').get('articles');
+
+  assert.equal(relationship.key, 'articles');
+  assert.equal(relationship.kind, 'hasMany');
 });
